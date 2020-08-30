2c0100ef72ce35f1e191fcc28ea5197671961e85
isaac ikusika <isaacikusika@gmail.com>
2020-08-30 14:23:34 +0100

    currently working with gmail

---
+++ Controllers/SendEmailController.cs
@@ -0,0 +1,53 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System;
+using System.Threading.Tasks;
+
+namespace EmailSender.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SendEmailController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("working");
+
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmail([FromBody] Email email)
+        {
+            string status;
+            try
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress(email.SenderName, email.AddressFrom));
+                message.To.Add(new MailboxAddress("", email.AddressTo));
+                message.Subject = email.Subject;
+
+                message.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+                {
+                    Text = email.Body
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    await client.ConnectAsync(email.SMTP, 465, true);
+                    //client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    // Note: only needed if the SMTP server requires authentication
+                    await client.AuthenticateAsync(email.Username, email.Password);
+
+                    await client.SendAsync(message);
+                    client.Disconnect(true);
+                    status = "success";
+                }
+            }
+            catch (Exception e)
+            {
+                status = e.Message;
+            }
+            
+            return Ok(status);
+        }
+    }
+}
---
+++ Email.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    public class Email
+    {
+        //String vFrom, String vTo, String vSubject, String vBody, String vSMTP, Int32 vPort, String vUserName, String vPassword
+        public string AddressFrom { get; set; }
+        public string  AddressTo { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string SMTP { get; set; }
+        public int Port { get; set; }
+
+        public string SenderName { get; set; }
+
+        //to use for authentication
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public Email()
+        {
+            Port = 587;
+        }
+    }
+}
